0bb92714698e0f72caeb790db14d4e0ee970277d
jobayercse71 <jobayercse71.com>
2022-04-01 23:24:45 +0600

    cartContainerAdd

---
+++ src/Layout/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css';
+
+const Cart = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Cart;
--- src/Sections/HomeSection/AllPlants.jsx
+++ src/Sections/HomeSection/AllPlants.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import cartImg from '../../img/cart.png';
+import favoriteImg from '../../img/favorite.png';
 import p1 from '../../img/p1.png';
+import searchImg from '../../img/search.png';
 import './AllPlants.css';
+
 
 const AllPlants = () => {
     return (
@@ -11,11 +15,35 @@ const AllPlants = () => {
                         <div className="product-media">
                             <img src={p1} className="w-100" height="250" alt="" />
 
+
+                            {/* cart hover section */}
+
+                            <div className="cart-hover-menu">
+                                <div className="d-flex cart-hover-content">
+                                    <div>
+                                        <img src={searchImg} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={favoriteImg} alt="" />
+                                    </div>
+                                    <div>
+                                        <img src={cartImg} alt="" />
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
+
+
+
                         <div>
                             <h6 className='product-title'>Beach Spider Lily</h6>
                             <h6 className='product-price'>$120</h6>
                         </div>
+
+
+
+
                     </div>
                 </div>
 
