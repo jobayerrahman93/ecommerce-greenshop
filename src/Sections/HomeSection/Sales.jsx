e97259c88eb23a2b88ec5cb0bcdb2544a8c66db8
jobayercse71 <jobayercse71.com>
2022-04-18 00:43:17 +0600

    sales-mongodbget-dynamic

--- src/Sections/HomeSection/Sales.jsx
+++ src/Sections/HomeSection/Sales.jsx
@@ -1,40 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import cartImg from '../../img/cart.png';
 import favoriteImg from '../../img/favorite.png';
-import p3 from '../../img/p3.png';
 import searchImg from '../../img/search.png';
 const Sales = () => {
+
+    const [salesProduct, setSalesProduct] = useState([]);
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/')
+            .then(res => res.json())
+            .then(data => {
+                const Singleproduct = data.filter(sales => sales.category == "sales");
+
+
+
+                setSalesProduct(Singleproduct);
+            });
+    }, []);
+
+    console.log(salesProduct);
+
     return (
         <>
             <div className="row mt-5">
-                <div className="col-lg-4">
-                    <div className="product-box">
-                        <div className="product-media">
-                            <img src={p3} className="" height="250" alt="" />
 
-                            {/* cart hover section */}
+                {
+                    salesProduct.map(singleSales =>
+                        <div className="col-lg-4">
 
-                            <div className="cart-hover-menu">
-                                <div className="d-flex cart-hover-content">
-                                    <div>
-                                        <img src={searchImg} alt="" />
+                            <div className="product-box">
+                                <Link to={`/productDetails/${singleSales.id}`}>
+                                    <div className="product-media">
+                                        <img src={singleSales.productImg} className="" height="250" alt="" />
+
+                                        {/* cart hover section */}
+
+                                        <div className="cart-hover-menu">
+                                            <div className="d-flex cart-hover-content">
+                                                <div>
+                                                    <img src={searchImg} alt="" />
+                                                </div>
+                                                <div>
+                                                    <img src={favoriteImg} alt="" />
+                                                </div>
+                                                <div>
+                                                    <img src={cartImg} alt="" />
+                                                </div>
+                                            </div>
+                                        </div>
+
                                     </div>
                                     <div>
-                                        <img src={favoriteImg} alt="" />
+                                        <h6 className='product-title'>
+                                            {singleSales.productName}
+                                        </h6>
+                                        <h6 className='product-price'>$  {singleSales.productPrice}</h6>
                                     </div>
-                                    <div>
-                                        <img src={cartImg} alt="" />
-                                    </div>
-                                </div>
+                                </Link>
                             </div>
+                        </div>
+                    )
+                }
 
-                        </div>
-                        <div>
-                            <h6 className='product-title'>Beach Spider Lily</h6>
-                            <h6 className='product-price'>$140</h6>
-                        </div>
-                    </div>
-                </div>
+
+
 
             </div>
         </>
