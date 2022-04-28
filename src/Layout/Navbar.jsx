33a11ac065d8cd7c8d8cd0b344535c6af6370469
jobayercse71 <jobayercse71.com>
2022-04-28 16:18:42 +0600

    name-change

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
               height="34"
               class="d-inline-block align-text-top me-2"
             />
-            Eco Shop
+            Green Shop
           </NavLink>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
