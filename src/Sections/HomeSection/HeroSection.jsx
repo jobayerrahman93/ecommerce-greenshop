3f0b9aa226355fcecd777a5819ef3ae67e6db686
jobayercse71 <jobayercse71.com>
2022-05-18 21:13:01 +0600

    banner-fixed

--- src/Sections/HomeSection/HeroSection.jsx
+++ src/Sections/HomeSection/HeroSection.jsx
@@ -17,44 +17,47 @@ const HeroSection = () => {
     };
     return (
         <>
-            <div className="container bg-light hero-section">
+            <div className="hero-section">
+                <div className="container bg-light ">
 
-                <Slider {...settings}>
-                    <div>
-                        <div className="row align-items-center">
-                            <div className="col-lg-6 col-8">
-                                <p className='hero-welcome'>Welcome to GreenShop</p>
-                                <h1 className='hero-title '>Let’s Make a
-                                    Better <span>Planet</span></h1>
-                                <p className='hero-subtitle'>We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!</p>
+                    <Slider {...settings}>
+                        <div className='single-hero-slide'>
+                            <div className="row align-items-center">
+                                <div className="col-lg-6 col-8">
+                                    <p className='hero-welcome'>Welcome to GreenShop</p>
+                                    <h1 className='hero-title '>Let’s Make a
+                                        Better <span>Planet</span></h1>
+                                    <p className='hero-subtitle'>We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!</p>
 
-                                <button type='button' className=' cmn-btn mt-2 mt-md-5 mt-lg-5 mb-5'>Shop Now</button>
-                            </div>
-                            <div className="col-lg-6 col-4">
-                                <img className='img-fluid' src={heroImg} height="518" alt="" />
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div>
-                        <div className="row  align-items-center">
-                            <div className="col-lg-6  col-8">
-                                <p className='hero-welcome'>Welcome to GreenShop</p>
-                                <h1 className='hero-title '>Let’s Make a
-                                    Better <span>work</span></h1>
-                                <p className='hero-subtitle'>We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!</p>
-
-                                <button type='button' className=' cmn-btn mt-2 mt-md-5 mt-lg-5 mb-5'>Shop Now</button>
-                            </div>
-                            <div className="col-lg-6 col-4">
-                                <img className='img-fluid' src={heroImg} height="518" alt="" />
+                                    <button type='button' className=' cmn-btn mt-2 mt-md-5 mt-lg-5 mb-5'>Shop Now</button>
+                                </div>
+                                <div className="col-lg-6 col-4">
+                                    <img className='img-fluid' src={heroImg} height="518" alt="" />
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                </Slider>
+
+                        <div className='single-hero-slide'>
+                            <div className="row  align-items-center">
+                                <div className="col-lg-6  col-8">
+                                    <p className='hero-welcome'>Welcome to GreenShop</p>
+                                    <h1 className='hero-title '>Let’s Make a
+                                        Better <span>work</span></h1>
+                                    <p className='hero-subtitle'>We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!</p>
+
+                                    <button type='button' className=' cmn-btn mt-2 mt-md-5 mt-lg-5 mb-5'>Shop Now</button>
+                                </div>
+                                <div className="col-lg-6 col-4">
+                                    <img className='img-fluid' src={heroImg} height="518" alt="" />
+                                </div>
+                            </div>
+                        </div>
+
+                    </Slider>
+                </div>
             </div>
+
         </>
     );
 };
